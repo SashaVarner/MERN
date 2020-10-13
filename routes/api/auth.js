b7bbac44b3e391f403c95b432c2f048def9d91b0
SashaVarner <sasha.pivowar@gmail.com>
2020-10-12 21:46:12 -0400

    Added routes for user profiles and users

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const User = require('../../models/User');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
 
 //@route       GET api/auth
 //@description Test route
@@ -19,6 +22,74 @@ router.get("/", auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+//@route       POST api/auth
+//@description Authenticate user and get token
+// @access     Public
+router.post(
+    "/",
+    [
+      check("email", "Please include a valid email").isEmail(),
+      check(
+        "password",
+        "Password is required"
+      ).exists(),
+    ],
+  
+    //we're doing async await
+    async (req, res) => {
+      //Because this is the object of data that is going to be sent to this route
+      //In order for this to work, we have to initialize the middleware for the  body parser
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() }); //Because we don't want to send a 200 we want to send a 400 which is a bad request
+        //Gives an array of errors
+      }
+  
+      const { email, password } = req.body;
+  
+      try {
+        //See if user exists (if it does, send an error)
+        let user = await User.findOne({ email });
+        if (!user) {
+          //if the user already exists
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "Invalid Credentials" }] });
+        }
+
+          const isMatch = await bcrypt.compare(password, user.password);
+
+          if (!isMatch) {
+              return res
+                  .status(400)
+              .json({errors: [{msg: 'Invalid Credentials'}]})
+          }
+  
+        const payload = {
+          user: {
+            id: user.id
+          }
+        };
+  
+        //the payload includes the username and information about the user.
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: '5 days' },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+  
+        //Server error
+      } catch (err) {
+        console.error(err.message); //So we can see it in the console
+        res.status(500).send("Server Error");
+      }
+    }
+  );
 
 //We need to export the route
 module.exports = router;
