68d1dc3f9dbee9f7b831c7bf2897b330a1c80686
SashaVarner <sasha.pivowar@gmail.com>
2020-10-17 19:05:39 -0400

    Updated posts and user files

--- models/User.js
+++ models/User.js
@@ -1,31 +1,26 @@
-//naming convention for models is an uppercase first letter
-//this is called a schema
-
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    //put all the attributes in here
-
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String, 
-        required: true,
-        unique: true // Two people can't use the same email
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    avatar: {
-        type: String
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = User = mongoose.model('user', UserSchema);
+module.exports = mongoose.model('user', UserSchema);
