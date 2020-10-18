847938c6b2ac117d8be4ed08abb6382a22b23df1
SashaVarner <sasha.pivowar@gmail.com>
2020-10-17 20:17:28 -0400

    Installed dependencies for react and deleted duplicate files created during React installation

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@
 This project is my final project for Towson University.
 
 To start it, type npm run server. (Make sure you're in the DevConnector folder)
+
+### `npm run dev`
+
+This is Sasha's created script that runs with concurrently. This makes it so that both the backend and the frontend can run at the same time (concurrently).
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,95 +1,80 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
-const config = require("config");
-const { check, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
-//@route       GET api/auth
-//@description Test route
-// @access     Public
+const User = require('../../models/User');
 
-//you can add the auth as an extra parameter
-router.get("/", auth, async (req, res) => {
-    //We want to return info about the user
-
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
-//@route       POST api/auth
-//@description Authenticate user and get token
-// @access     Public
+// @route    POST api/auth
+// @desc     Authenticate user & get token
+// @access   Public
 router.post(
-    "/",
-    [
-      check("email", "Please include a valid email").isEmail(),
-      check(
-        "password",
-        "Password is required"
-      ).exists(),
-    ],
-  
-    //we're doing async await
-    async (req, res) => {
-      //Because this is the object of data that is going to be sent to this route
-      //In order for this to work, we have to initialize the middleware for the  body parser
-      const errors = validationResult(req);
-      if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() }); //Because we don't want to send a 200 we want to send a 400 which is a bad request
-        //Gives an array of errors
-      }
-  
-      const { email, password } = req.body;
-  
-      try {
-        //See if user exists (if it does, send an error)
-        let user = await User.findOne({ email });
-        if (!user) {
-          //if the user already exists
-          return res
-            .status(400)
-            .json({ errors: [{ msg: "Invalid Credentials" }] });
-        }
-
-          const isMatch = await bcrypt.compare(password, user.password);
-
-          if (!isMatch) {
-              return res
-                  .status(400)
-              .json({errors: [{msg: 'Invalid Credentials'}]})
-          }
-  
-        const payload = {
-          user: {
-            id: user.id
-          }
-        };
-  
-        //the payload includes the username and information about the user.
-        jwt.sign(
-          payload,
-          config.get("jwtSecret"),
-          { expiresIn: '5 days' },
-          (err, token) => {
-            if (err) throw err;
-            res.json({ token });
-          }
-        );
-  
-        //Server error
-      } catch (err) {
-        console.error(err.message); //So we can see it in the console
-        res.status(500).send("Server Error");
-      }
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-  );
 
-//We need to export the route
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
+
 module.exports = router;
